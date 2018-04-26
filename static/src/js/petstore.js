376981404b18846366f22fe5f65b956623ce4bc0
shubhamj <shubhamjohar024@gmail.com>
2018-04-26 11:04:18 +0530

    [2] Changed previous JS code to support Odoo 12

--- static/src/js/petstore.js
+++ static/src/js/petstore.js
@@ -11,3 +11,22 @@
 
 //     instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
 // }
+
+odoo.define('oepetstore', function(require){
+    'use strict';
+
+    var AbstractAction = require('web.AbstractAction');
+    var Widget = require('web.Widget');
+    var WebClient = require('web.WebClient');
+    var core = require('web.core');
+
+    console.log(" PETSTORE.JS loaded");
+
+    var HomePage = AbstractAction.extend({
+        start : function(){
+            console.log("pet store home page loaded");
+        }
+    });
+
+    core.action_registry.add('petstore.homepage',HomePage);
+})
